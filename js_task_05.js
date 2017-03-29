6a5ef698e722a122d7198e4dedec1dfa43d8d99b
luanqi521 <cupluanqi@sina.com>
2017-03-29 14:23:19 +0800

    Update js_task_05.js

--- js_task_05.js
+++ js_task_05.js
@@ -4,24 +4,50 @@
     var rightin = document.getElementById("rightin");
     var leftout = document.getElementById("leftout");
     var rightout = document.getElementById("rightout");
+    var sort = document.getElementById("sort");
     var numlist = document.getElementById("numDisplay");
 
     function setDivHeight() {
-        var item = document.createElement("div");
-        var li = document.createElement("li");
-        li.style.height = String((num.value) * 2) + "px";
-        item.appendChild(li);
-        return item;
+        if ((num.value < 10) || (num.value > 100)) {
+            alert("请输入10-100之间的数字");
+            return null;
+        }
+        else {
+            var item = document.createElement("div");
+            var item1 = document.createElement("div");
+            item1.style.height = String((num.value) * 2) + "px";
+            item1.innerHTML = num.value;
+            item.appendChild(item1);
+            return item;
+        }
     }
+
     function Leftin() {
+        debugger;
         var item = setDivHeight();
-        numlist.insertBefore(item, numlist.firstChild);
-        num.value = "";
+        var itemlist = numlist.getElementsByTagName("div");
+        if (itemlist.length <= 60) {
+            numlist.insertBefore(item, numlist.firstChild);
+            num.value = "";
+        }
+        else {
+            alert("队列元素数量最多限制为60个");
+            num.value = "";
+        }
+
     }
     function Rightin() {
         var item = setDivHeight();
-        numlist.appendChild(item);
-        num.value = "";
+        var itemlist = numlist.getElementsByTagName("div");
+        if (itemlist.length <= 60) {
+            numlist.appendChild(item);
+            num.value = "";
+        }
+        else {
+            alert("队列元素数量最多限制为60个");
+            num.value = "";
+        }
+
     }
     function Leftout() {
         var itemlist = numlist.getElementsByTagName("div");
@@ -33,6 +59,20 @@
         if (confirm("确定要删除" + itemlist[itemlist.length - 1] + "吗?"))
             itemlist[itemlist.length - 1].remove();
     }
+
+    //从小到大排序
+    function Sort() {
+        var itemlist = document.getElementsByTagName("div");
+        for (var i = 0; i < itemlist.length; i++)
+            for (var j = i + 1; j < itemlist.length; j++) {
+                if (itemlist[j] < itemlist[i]) {
+                    var t = itemlist[j];
+                    itemlist[j] = itemlist[i];
+                    itemlist[i] = itemlist[j];
+                }
+            }
+    }
+
     leftin.onclick = function () {
         Leftin();
     }
@@ -45,5 +85,9 @@
     rightout.onclick = function () {
         Rightout();
     }
+    sort.onclick = function () {
+        Sort();
+    }
+
 }
 )();
